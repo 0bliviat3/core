f06fddc2ee006d7a8808742022f1566639af038b
songjaewan <sjo9810@naver.com>
2024-04-01 23:00:20 +0900

    같은 타입의 빈이 2개 이상인 경우:
    AutoWired 애노테이션이 붙은 필드 또는 생성자의
    변수명에 특정 빈을 명시해준다.
    
    DiscountPolicy 타입의 빈이 2개이므로
    autoWired를 통해 자동주입이 안되는 상황이므로
    생성자의 파리미터를 DiscountPolicy fixDiscountPolicy 로 수정해줌으로서 스프링이
    빈을 찾을때 이름을보고 찾을 수 있다.
    
    탐색순서: 1. 타입명 2. 변수명
    
    @Qualifier를 사용하면
    각각의 빈에 구체적인 이름을 지정해줄수 있고
    이를 통해 빈을 구분해줄수 있다.
    
    탐색순서: 1. Qualifier끼리 매칭 2. 빈이름 탐색
    
    @Primary
    동일 타입의 빈중에서 특정 빈에 이 애노테이션을 달아주면 우선순위를 갖고 특정빈을 우선 의존관계주입한다.
    
    @primary와 @Qualifier를 적절히 활용하면된다.
    메인이되는 빈의 경우 primary를 사용
    서브가되는 빈의 경우 Qualifier를 사용해 명시적으로 사용한다.
    
    우선순위는 Qualifier가 더 높다.
    (당연하게도 primary는 기본값으로 사용하는것이고
    Qualifier를 사용하면 명시적으로 지정해주기 때문)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,15 +3,19 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+
+    public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override
