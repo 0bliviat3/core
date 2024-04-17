99fbb83fdb7ce358d8c4a28c37bc7f522ae4b4c8
songjaewan <sjo9810@naver.com>
2024-04-17 22:37:00 +0900

    싱글톤 스코프와 프로토타입 스코프의 동시사용1:
    싱글톤 빈 내부에서 프로토타입 빈을 주입받아 사용하면 싱글통 빈생성시 프로토타입 빈도 초기에 하나의 빈만 주입받아 사용되므로 덩달아 싱글톤으로 강제하게 된다.
    이는 의도한 바가 아니다
    의도한 바는 클라이언트의 요청시마다 프로토타입 빈을 새로 생성하는것이 목적이므로 동시 사용시에는
    다른 방법을 사용해야 한다.

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,81 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCnt();
+        assertThat(prototypeBean1.getCnt()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCnt();
+        assertThat(prototypeBean2.getCnt()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int cnt1 = clientBean1.logic();
+        assertThat(cnt1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int cnt2 = clientBean2.logic();
+        assertThat(cnt2).isEqualTo(2);
+
+        assertThat(clientBean1).isSameAs(clientBean2);
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int cnt = 0;
+
+        public void addCnt() {
+            cnt++;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+    @Scope("singleton") //생략 가능
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(final PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCnt();
+            return prototypeBean.getCnt();
+        }
+    }
+}
