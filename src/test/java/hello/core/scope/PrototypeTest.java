afcf8d03355e5160fc0a94224e21ec6768553168
songjaewan <sjo9810@naver.com>
2024-04-17 22:11:37 +0900

    빈 스코프:
    싱글톤 스코프 - 기본스코프, 스프링컨테이너의 시작과 끝까지 유지되는 가장 넓은 범위의 스코프이다.
    프로토타입 - 스프링 컨테이너는 프로토타입의 빈의 생성과 의존관계 주입까지만 관여하고 더는 관리하지 않는 범위의 스코프
    웹관련스코프 - request, session, application

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close(); //생성된 시점부터 더는 관리하지 않는다.
+
+        prototypeBean1.close();// 소멸이 필요한경우 직접 객체를 관리해주어야 한다.
+        prototypeBean2.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
